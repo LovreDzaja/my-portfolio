e9f12d37ca546f1e9d295eca759644781ee2465e
you <you@example.com>
2024-07-19 21:06:51 +0200

    Added analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import {Route, BrowserRouter as Router, Routes} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import { Home, About, Projects } from "./pages";
 import FollowCursor from './components/FollowCursor';
+import { Analytics } from "@vercel/analytics/react"
 
 const App = () => {
   return (
     <main className='bg-slate-300/20'>
+      <Analytics/>
       <FollowCursor/>
       <Router>
         <Navbar/>
